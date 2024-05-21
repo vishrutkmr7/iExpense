a0f2dd3da227e7c2345194f7bd2626007f1e8eb6
Vishrut Jha <vishrutkmr7@icloud.com>
2024-05-21 00:15:02 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Vishrut Jha on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
